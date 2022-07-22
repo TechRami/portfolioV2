674b6c8c292f69c9445f212b0a082350672fba7e
TechRami <techrami@outlook.com>
2022-07-22 21:55:30 +0300

    fixing padding on section A

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,16 +18,8 @@ const animateBtn = () =>{
 menuBtn.addEventListener('click', animateBtn)
 
 
-window.onscroll = () => {
-    if (window.scrollY >= 150 && window.scrollY <= 249) {
-        navbar.classList.add('scrolled');
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].style.color = '#7963FF'
-        }
-        logo.style.fill = '#7963FF'
-        for (let i = 0; i < menuLines.length; i++) {
-            menuLines[i].style.backgroundColor = '#696969'
-        }
-        
-    }
-}
+// window.onscroll = () => {
+//     if (window.scrollY >= 150 && window.scrollY <= 249) {
+//         navbar.classList.add('scrolled');
+//     }
+// }
